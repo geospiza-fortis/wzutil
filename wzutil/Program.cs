9b19fe9fad41d167749b8a48c6ff2cab9cc565d6
geospiza <geospiza@protonmail.com>
2020-11-03 20:25:33 -0800

    Add initial project for alternative dumping of wz files

---
+++ wzutil/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using MapleLib.WzLib;
+using MapleLib.WzLib.Serialization;
+
+
+namespace JsonExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Refer to MainForm::RunWzFilesExtraction
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Wrong number of arguments.");
+                Console.WriteLine("Usage: <WZ File> <Output Directory>");
+                System.Environment.Exit(1);
+            }
+            var wzPath = args[0];
+            var baseDir = args[1];
+            Console.WriteLine($"Exporting {wzPath} to {baseDir}");
+            WzMapleVersion version = WzMapleVersion.GMS;
+            IWzFileSerializer serializer = new WzClassicXmlSerializer(2, LineBreak.Windows, false);
+
+            if (!Directory.Exists(baseDir))
+            {
+                Directory.CreateDirectory(baseDir);
+            }
+            WzFile wz = new WzFile(wzPath, version);
+            wz.ParseWzFile();
+            serializer.SerializeFile(wz, Path.Combine(baseDir, wz.Name));
+            wz.Dispose();
+        }
+    }
+}
